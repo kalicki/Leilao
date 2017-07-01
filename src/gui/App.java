81cc3a9a79221fd24e5c0f94a944af6e06da7152
Filipe F. Kalicki <filipe.kalicki@gmail.com>
2017-06-30 23:18:51 -0300

    Adicionado GUI

--- src/gui/App.java
+++ src/gui/App.java
@@ -11,9 +11,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Leilão");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
